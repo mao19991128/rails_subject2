c6825ba47643fba0344e2d0548947c213f741745
mao19991128 <mao19991128@icloud.com>
2023-03-07 14:48:33 +0900

    users/showページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'reservations/index'
   get 'rooms/index'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :rooms
   resources :reservations
+  get "users/show" => "users#show"
 end
